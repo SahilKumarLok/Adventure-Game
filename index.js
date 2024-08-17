49818b292d7d44b11378bbf3990b1e9f94dd7ab4
SahilKumarLok <78581440+SahilKumarLok@users.noreply.github.com>
2024-08-17 20:53:44 +0500

    completed

---
+++ index.js
@@ -0,0 +1,66 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+class Character {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    decreaseFuel() {
+        this.fuel -= 25;
+    }
+}
+class Player extends Character {
+    constructor(name) {
+        super(name);
+    }
+}
+class Opponent extends Character {
+    constructor(name) {
+        super(name);
+    }
+}
+async function main() {
+    const player = await inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Please Enter Your Name: "
+        }
+    ]);
+    const opponent = await inquirer.prompt([
+        {
+            name: "Select",
+            type: "list",
+            message: "Select Your Opponent",
+            choices: ["Alien", "Zombie"],
+        }
+    ]);
+    const p1 = new Player(player.name);
+    const o1 = new Opponent(opponent.Select);
+    while (true) {
+        const ask = await inquirer.prompt([
+            {
+                name: "opt",
+                type: "list",
+                message: "What would you like to do",
+                choices: ["Attack", "Run"],
+            }
+        ]);
+        if (ask.opt === "Attack") {
+            const num = Math.floor(Math.random() * 2);
+            if (num > 0) {
+                p1.decreaseFuel();
+            }
+            else {
+                o1.decreaseFuel();
+            }
+            console.log(`${p1.name} fuel is ${p1.fuel}`);
+            console.log(`${o1.name} fuel is ${o1.fuel}`);
+        }
+        else if (ask.opt === "Run") {
+            console.log(`${p1.name} is running away from ${o1.name}`);
+        }
+    }
+}
+main();
